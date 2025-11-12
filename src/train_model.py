a11c8857db8867a1307d2487cef8c1b9d9080294
monarchxr <raunakjais2131@gmail.com>
2025-11-12 22:53:35 +0530

    ending with using random forest, and also saving the model and scaler

--- src/train_model.py
+++ src/train_model.py
@@ -4,6 +4,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 
 df = pd.read_csv("data/raw/pd.csv")
 
@@ -19,7 +21,7 @@ x_scaled = scaler.fit_transform(x)
 
 x_train, x_test, y_train, y_test = train_test_split(x_scaled,y,test_size=0.2, random_state=42)
 
-model = LogisticRegression(max_iter=1000, class_weight='balanced')
+model = RandomForestClassifier(n_estimators=200, max_depth=None, random_state=42, class_weight='balanced')
 model.fit(x_train,y_train)
 
 y_pred = model.predict(x_test)
@@ -35,3 +37,9 @@ print("\nClassification Report:\n", classification_report(y_test, y_pred))
 cv_scores = cross_val_score(model, x_scaled, y, cv=5, scoring='accuracy')
 print(f"CV scores: {cv_scores}")
 print(f"average cv: {cv_scores.mean():.3f} (+/- {cv_scores.std()*2:.3f})")
+
+with open("models/model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+with open("models/scaler.pkl", "wb") as f:
+    pickle.dump(scaler, f)
